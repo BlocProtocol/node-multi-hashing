e403823787386988bd689837911e6f64ad90cba3
jpascal4 <38017149+jpascal4@users.noreply.github.com>
2018-07-10 16:02:38 +0100

    type

--- multihashing.cc
+++ multihashing.cc
@@ -476,7 +476,7 @@ NAN_METHOD(cryptonight_heavy) {
 	{
 		const char dig[16] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
 		dbg += dig[uint8_t(output[i]) >> 4];
-		dgb += dig[uint8_t(output[i]) & 0x0f];
+		dbg += dig[uint8_t(output[i]) & 0x0f];
 	}
 
 	return THROW_ERROR_EXCEPTION(dbg.c_str());
